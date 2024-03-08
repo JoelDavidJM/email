19c18a09d32d6ca859538494cf8e5c28ef763446
Joel <jodajativa17@gmail.com>
2024-03-07 20:36:52 -0500

    email

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const catchError = require('../utils/catchError')
+const sendEmail = require('../utils/sendEmail')
 
 // colocar las rutas aquí
-
+router.post("/emails", catchError(async (req, res) => {
+    await sendEmail({
+        to: "joel@gmail.com",
+        subject: "proyectod",
+        html: `
+            <h1>Hola mundo</h1>
+            <p>lorem ipsum dolor</p> 
+        `
+    });
+    return res.json({ message: "enviando email" });
+}));
 
 module.exports = router;
